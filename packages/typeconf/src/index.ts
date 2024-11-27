82df8f11b2b484e8ebf2985e8e410fcb58c63cee
Ivan Chebykin <ivan@chebykin.org>
2024-11-27 02:01:43 +0000

    add logging

--- packages/typeconf/src/index.ts
+++ packages/typeconf/src/index.ts
@@ -3,25 +3,34 @@ import { Command } from "commander";
 import { compile } from "./compile/compile.js";
 import initProject from "./init.js";
 import { updateConfig } from "./update.js";
+import { log_event } from "./logging.js";
 
 const program = new Command();
+export const VERSION = "0.1.0";
 
 program
-  .version("0.1.0")
+  .version(VERSION)
   .description("A CLI tool for writing configs with types");
 
 program
   .command("init [directory]", { isDefault: false })
   .description("Init new configuration package")
   .action(async (directory) => {
+    const params: Record<string, string> = {
+        "configDir": directory,
+    };
+    log_event("info", "init", "start", params);
     await initProject(directory ?? process.cwd());
+    log_event("info", "init", "end", params);
   });
 
 program
   .command("update", { isDefault: false })
   .description("Update config types in your project")
   .action(async () => {
+    log_event("info", "update", "start");
     await updateConfig();
+    log_event("info", "update", "end");
   });
 
 program
@@ -32,8 +41,14 @@ program
     false,
   )
   .description("Compile the configuration package")
-  .action(async (configDir: string) => {
+  .action(async (configDir: string, options: any) => {
+    const params: Record<string, string> = {
+        "configDir": configDir,
+        "watch": (options?.watch ?? false).toString(),
+    };
+    log_event("info", "compile", "start", params);
     await compile(configDir);
+    log_event("info", "compile", "end", params);
   });
 
 program.parse(process.argv);
---
+++ packages/typeconf/src/logging.ts
@@ -0,0 +1,43 @@
+import { createClient } from "@supabase/supabase-js";
+import { VERSION } from "./index.js";
+
+const LOG_URL = "https://sfcheddgbldthfcxoaqn.supabase.co";
+const LOG_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmY2hlZGRnYmxkdGhmY3hvYXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3OTM1MzgsImV4cCI6MjA0NzM2OTUzOH0.pErXNTPwqK71LA-mC3tfZdPtE8rYySyaOo1czW-MpEs";
+
+type LogLevel = "info" | "error" | "warning";
+
+export class LogArgs {
+  level: LogLevel = "info";
+  params: Record<string, string> = {};
+  command: string = "";
+  message: string = "";
+}
+
+export function log_event(
+  level: LogLevel,
+  command: string,
+  message: string,
+  params: Record<string, string> = {},
+): void {
+  log_event_impl({
+    level: level,
+    params: params,
+    command: command,
+    message: message,
+  });
+}
+
+async function log_event_impl(args: LogArgs): Promise<void> {
+  const supabase = createClient(LOG_URL, LOG_ANON_KEY);
+  const { data, error } = await supabase.from("logs").upsert({
+    user_id: "dev",
+    level: args.level,
+    ts: new Date().toISOString(),
+    version: VERSION,
+    params: args.params,
+    command: args.command,
+    message: args.message,
+  });
+  console.log(`${data} ${error?.message}`);
+}
